f667243e8e6a57452e32a97b5469a1ff36f0c5f4
Rex <a0916996002@gmail.com>
2024-05-24 11:48:27 +0800

    Add: 預處理器 (巨集) 防止多次引入時，造成多次包含並導致重複定義

--- dataStructure_C++/array/vector_arr.h
+++ dataStructure_C++/array/vector_arr.h
@@ -9,11 +9,30 @@
  * @details 使用容器操作陣列資料
  */
 
+
+// 若第一次引入本檔案，則進入 #define 先定義 __VECTOR_ARR__
+// 
+// 反之，第二次或二次以上，則 #ifndef 會不成立，因為已經定義過 __VECTOR_ARR__
+// 因此，會被跳過整個 #ifndef 到 #endif 之間的區塊的內容，從而避免了重複定義
+// 
+#ifndef __VECTOR_ARR__
+#define __VECTOR_ARR__
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <regex>
+
 
 using namespace std;
+
+
+// template<typename T>
+// int get_number_of_digits(T number) {
+//     if (isalpha())
+// }
+
+
 
 // ============================================================================
 // ============================================================================
@@ -21,31 +40,70 @@ using namespace std;
 
 
 /**
+ * @brief print 1 dimension vector (1D vector)
+ * 
+ * @param array_1D 1D vector
+ */
+template<typename T>
+void print_vector(vector<T>& array_1D) {
+    cout<<"{ ";
+    
+    if (array_1D.size() == 0) {
+        cout<<" }"<<endl;
+        return;
+    }
+
+    for (int i = 0; i < array_1D.size(); i++) {
+        if (i < array_1D.size() - 1) {
+            cout<<array_1D[i]<<", ";
+        } else {
+            cout<<array_1D[i]<<" }";
+        }
+    }
+    cout<<endl;
+}
+
+
+/**
  * @brief print 2 dimensional vector (2D vector)
  * 
  * @param array_2D 2D vector
- * @param cut_line 切分之每組數量
+ * @param cut_line 切分之每組數量，附上行號
+ * 
  */
-template<typename dtype>
-void print_vector(vector<vector<dtype>>& array_2D, int cut_line=0) {
+template<typename T>
+void print_vector(vector<vector<T>>& array_2D, int cut_line = 0) {
     cout<<"{";
+    
+    if (array_2D.size() == 0 || array_2D[0].size() == 0) {
+        cout<<"{ }}"<<endl;
+        return;
+    }
+    
     for (int i = 0; i < array_2D.size(); i++) {
-        cout<<"{";
-        for (int j = 0; j < array_2D[0].size(); j++) {
-            if (j < array_2D[0].size() - 1) {
-                cout<<array_2D[i][j]<<", ";
-            } else {
+        cout << "{ ";
+        
+        for (int j = 0; j < array_2D[i].size(); j++) {
+            if (j < array_2D[i].size() - 1) {
+                cout << array_2D[i][j] << ", ";
+            }
+            
+            else {
                 if (i < array_2D.size() - 1) {
-                    cout<<array_2D[i][j]<<"}, ";
+                    cout << array_2D[i][j] << " }, ";
+                    
                     if (cut_line > 0 && i % cut_line == cut_line - 1) {
-                        cout<<endl;
+                        cout<<endl<<endl;
                         cout<<">----------------------< ("<<i+1<<") line"<<endl;
                     }
                     cout<<endl<<" ";
-                } else {
-                    cout<<array_2D[i][j]<<"}}";
+                }
+                
+                else {
+                    cout << array_2D[i][j] << " }}";
+                    
                     if (cut_line > 0) {
-                        cout<<endl;
+                        cout<<endl<<endl;
                         cout<<">----------------------< ("<<i+1<<") line"<<endl;
                     }
                 }
@@ -56,34 +114,16 @@ void print_vector(vector<vector<dtype>>& array_2D, int cut_line=0) {
 }
 
 
-/**
- * @brief print 1 dimension vector (1D vector)
- * 
- * @param array_1D 1D vector
- */
-template<typename dtype>
-void print_vector(vector<dtype>& array_1D) {
-    cout<<"{";
-    for (int i = 0; i < array_1D.size(); i++) {
-        if (i < array_1D.size() - 1) {
-            cout<<array_1D[i]<<", ";
-        } else {
-            cout<<array_1D[i]<<"}";
-        }
-    }
-    cout<<endl;
-}
-
 
 /**
  * @brief 遞增給數值
  * 
  * @tparam T 
- * @param array_2D 2D vector
+ * @param array_2D 2D vector  (m * n)
  * @param start 起始數值
  */
 template<typename T>
-void arange(vector<vector<T>>& array_2D, T start=0) {
+void arange(vector<vector<T>>& array_2D, T start = 0, bool _reverse = false) {
     int row = array_2D.size();
     int col = array_2D[0].size();
 
@@ -92,11 +132,40 @@ void arange(vector<vector<T>>& array_2D, T start=0) {
     }
 
     for (int i = 0; i < row; i++) {
+        col = array_2D[i].size();
+
         for (int j = 0; j < col; j++) {
             array_2D[i][j] = start;
             start++;
         }
+
+        if (_reverse == true) {
+            reverse(array_2D[i].begin(), array_2D[i].end());
+        }
     }
+
+    if (_reverse == true) {
+        reverse(array_2D.begin(), array_2D.end());
+    }
+}
+
+
+template<typename T>
+void print_vector(vector<vector<vector<T>>>& array_3D) {
+    cout << "{" <<endl;
+    
+    for (auto vec_2d : array_3D) {
+        cout << "\t{" << endl;
+        
+        for (auto vec : vec_2d) {
+            cout << "\t\t";
+            print_vector<int>(vec);
+        }
+
+        cout << "\t}" <<endl;
+    }
+
+    cout << "}" <<endl;    
 }
 
 
@@ -109,7 +178,7 @@ void arange(vector<vector<T>>& array_2D, T start=0) {
  */
 
 template<typename T>
-void arange(vector<T>& array_1D, T start=0) {
+void arange(vector<T>& array_1D, T start=0, bool _reverse = false) {
     int row = array_1D.size();
 
     if (row <= 0) {
@@ -120,86 +189,11 @@ void arange(vector<T>& array_1D, T start=0) {
         array_1D[i] = start;
         start++;
     }
-}
 
-
-// ============================================================================
-// ============================================================================
-// ============================================================================
-
-
-template<typename dtype>
-class VectorArray {
-private:
-    int row;
-    int col;
-
-public:
-    vector<dtype> array_1D;
-    vector<vector<dtype>> array_2D;
-
-    VectorArray(vector<dtype>& arr_1D) {
-        row = arr_1D.size();
-
-        this->array_1D = arr_1D;
-    }
-
-    VectorArray(vector<vector<dtype>>& arr_2D) {
-        row = arr_2D.size();
-        col = arr_2D[0].size();
-        this->array_2D = arr_2D;
-    }
-
-    vector<dtype> shape() {
-        vector<int> shape = {this->row};
-        if (col >= 0) {
-            shape.push_back(this->col);
-            return shape;
-        }
-
-        return shape;
-    }
-
-    // =========================================================
-    // =========================================================
-    // =========================================================
-
-    // 一維遞增
-    // 二維遞增
-    // param: start: 起始數值
-    // param: increase: 遞增開關，預設: 開
-    void arange(dtype start = 0, bool increase=true);
-
-};
-
-
-template<typename dtype>
-void VectorArray<dtype>::arange(dtype start, bool increase) {
-    if (col > 0) {
-        for (int i = 0; i < row; i++) {
-            vector<dtype> arr;
-            for (int j = 0; j < col; j++) {
-                arr.push_back(start);
-                if (increase) {
-                    start++;
-                }
-            }
-            array_2D.push_back(arr);
-            arr.~vector();   // 清除原資料
-        }
-
-        // print_vector<dtype>(array_2D);
-    }
-    else {
-        for (int i = 0; i < row; i++) {
-            array_1D.push_back(start);
-            start++;
-        }
-        // print_vector<dtype>(array_1D);
+    if (_reverse == true) {
+        reverse(array_1D.begin(), array_1D.end());
     }
 }
 
 
-
-
-
+#endif
