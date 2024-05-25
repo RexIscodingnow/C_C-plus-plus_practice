9d6480fa491edc0a2c99a5f573f39bba4753fec7
Rex <a0916996002@gmail.com>
2024-05-25 19:23:28 +0800

    Add: print all disks moving procedure

--- algorithm/divide_and_conquer/hanoi_tower.cpp
+++ algorithm/divide_and_conquer/hanoi_tower.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 void hanoi_towers(int disks, char from, char aux, char end);
 
-void move(vector<int> &src, vector<int> &tar);
+void move(vector<int> &src, vector<int>& buf, vector<int> &tar);
 void dfs(int i, vector<int> &src, vector<int> &buf, vector<int> &tar);
 
 
@@ -15,22 +15,27 @@ int main(int argc, char const *argv[]) {
 
     vector<int> disks(3, {0}), buf, tar;
 
-    arange<int>(disks, 1, true);
+    arange<int>(disks, 1, true);    // from 1 ~ n
     
     cout << "1 ~ " << disks.size() << " :" << endl << endl;
-
-    cout << "A : ";
-
-    print_vector<int>(disks);
+    
+    cout << "A (src) : ";
+    print_vector(disks);
+    cout << "B (buf) : ";
+    print_vector(buf);
+    cout << "C (tar) : ";
+    print_vector(tar);
     cout << "------------------------------" << endl << endl;
 
     dfs(disks.size(), disks, buf, tar);
 
     cout << "------------------------------" << endl << endl;
-    cout << "A : ";
-    print_vector<int>(disks);
-    cout << "C : ";
-    print_vector<int>(tar);
+    cout << "A (src) : ";
+    print_vector(disks);
+    cout << "B (buf) : ";
+    print_vector(buf);
+    cout << "C (tar) : ";
+    print_vector(tar);
 
     
     return 0;
@@ -52,9 +57,14 @@ void hanoi_towers(int disks, char from, char aux, char end) {
 	}
 }
 
-
-/* 移動一個圓盤 */
-void move(vector<int> &src, vector<int> &tar) {
+/**
+ * @brief 移動一個圓盤
+ * 
+ * @param src 起始點
+ * @param buf 暫存點 (無移動操作，觀察移動過程)
+ * @param tar 終點
+ */
+void move(vector<int> &src, vector<int>& buf, vector<int> &tar) {
     // 從 src 頂部拿出一個圓盤
     int pan = src.back();
 
@@ -62,44 +72,40 @@ void move(vector<int> &src, vector<int> &tar) {
     
     // 將圓盤放入 tar 頂部
     tar.push_back(pan);
+
+    // 印出當前狀態
+    cout << "A : ";
+    print_vector(src);
+    cout << "B : ";
+    print_vector(buf);
+    cout << "C : ";
+    print_vector(tar);
+    cout << "------------------------------" << endl;
 }
 
-/* 求解河內塔問題 f(i) */
+
+/**
+ * @brief 求解河內塔問題 f(i)
+ * 
+ * @param i   剩餘數量
+ * @param src 起始點
+ * @param buf 中繼點 (暫存點)
+ * @param tar 終點
+ */
 void dfs(int i, vector<int> &src, vector<int> &buf, vector<int> &tar) {
     // 若 src 只剩下一個圓盤，則直接將其移到 tar
     if (i == 1) {
-        move(src, tar);
+        move(src, buf, tar);
         return;
     }
 
-    cout << "A : ";
-    print_vector<int>(src);
-    cout << "B : ";
-    print_vector<int>(buf);
-    cout << "C : ";
-    print_vector<int>(tar);
-
     // 子問題 f(i-1) ：將 src 頂部 i-1 個圓盤藉助 tar 移到 buf
     dfs(i - 1, src, tar, buf);
-    
-    cout << "A : ";
-    print_vector<int>(src);
-    cout << "B : ";
-    print_vector<int>(buf);
-    cout << "C : ";
-    print_vector<int>(tar);
 
     // 子問題 f(1) ：將 src 剩餘一個圓盤移到 tar
-    move(src, tar);
+    move(src, buf, tar);
 
     // 子問題 f(i-1) ：將 buf 頂部 i-1 個圓盤藉助 src 移到 tar
     dfs(i - 1, buf, src, tar);
-
-    cout << "A : ";
-    print_vector<int>(src);
-    cout << "B : ";
-    print_vector<int>(buf);
-    cout << "C : ";
-    print_vector<int>(tar);
 }
 
